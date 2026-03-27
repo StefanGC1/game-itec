5b94210239d5f051f60a58ba38ed8fbf223947a9
stefan <stefan@pop-os.upt.intra>
2026-03-28 00:14:46 +0200

    Started work on 2D cave system, create test level, and implemented ZLayerSwitch functionality

--- features/player/player2d/player_2d.gd
+++ features/player/player2d/player_2d.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -900
+const GRAVITY = 900.0
 
+@onready var foot_pos_marker = $FootPosition
+
+signal switch_layer(direction: int, player_position: Vector2)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += GRAVITY * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("2d_jump") and is_on_floor():
@@ -23,3 +26,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("2d_layer_forward"):
+		emit_signal("switch_layer", 1, foot_pos_marker.global_position)
+	elif event.is_action_pressed("2d_layer_backward"):
+		emit_signal("switch_layer", -1, foot_pos_marker.global_position)
